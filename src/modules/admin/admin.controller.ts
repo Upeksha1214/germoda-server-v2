6491bbf49da5a3f81b36a5631a19a4af2c5fa741
AnupamaCodippily <avcodippily@gmail.com>
2023-02-09 17:35:34 +0530

    set authguard to controller level admin

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -13,29 +13,26 @@ import { AdminService } from './admin.service';
 import CreateAdminDTO from './dto/create-admin.dto';
 import UpdateAdminDTO from './dto/update-admin.dto';
 
+@UseGuards(AuthGuard(ADMIN_AUTH_JWT))
 @Controller('api/admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
-  @UseGuards(AuthGuard(ADMIN_AUTH_JWT))
   @Get('/:id')
   getAdminById(@Param('id') adminId: string) {
     return this.adminService.findById(adminId);
   }
 
-  @UseGuards(AuthGuard(ADMIN_AUTH_JWT))
   @Get()
   getAdminList() {
     return this.adminService.findAll();
   }
 
-  @UseGuards(AuthGuard(ADMIN_AUTH_JWT))
   @Post()
   addNewAdmin(@Body() createAdminDto: CreateAdminDTO) {
     return this.adminService.createNewAdmin(createAdminDto.adminDetails);
   }
 
-  @UseGuards(AuthGuard(ADMIN_AUTH_JWT))
   @Put(':id')
   updateAdmin(
     @Param('id') adminId: string,
